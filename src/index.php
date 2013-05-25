bc1ac92aae8034ef6634c92d724f4fd8c312523b
tera911.mainPC <t_hanyu_kudo@yahoo.co.jp>
2013-05-25 23:56:28 +0900

    fixed

--- src/index.php
+++ src/index.php
@@ -1,8 +1,8 @@
 <?php 
 	session_start();
-	if(empty($_SESSION['id']) || empty($_SESSION['auth'])){
+	/*if(empty($_SESSION['id']) || empty($_SESSION['auth'])){
 		header('Location: ./clearsession.php');
-	}
+	}*/
 
 	require_once('./pages/top.html');
 ?>
